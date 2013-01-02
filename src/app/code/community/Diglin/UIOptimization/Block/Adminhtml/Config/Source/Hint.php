572fb251812e8c90e6a59122b1ebca0216a78285
Sylvain Rayé <sly@diglin.com>
2013-01-02 22:33:05 +0100

    Update version number and a text

--- src/app/code/community/Diglin/UIOptimization/Block/Adminhtml/Config/Source/Hint.php
+++ src/app/code/community/Diglin/UIOptimization/Block/Adminhtml/Config/Source/Hint.php
@@ -37,6 +37,6 @@ class Diglin_UIOptimization_Block_Adminhtml_Config_Source_Hint
                 ));
                 
         return '<p><a href="javascript:window.open(\'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y66QHLU5VX5BC\')"><img src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" alt="Donate via Paypal" /> </a>Please, Invite me for a drink for the hard work done. Thank you in advance for your donation</p>
-        <p><strong>Diglin_Chat Version: '.$version.'</strong></p><p>' . $block->toHtml() .' ('. $this->__('You will be redirected to the Cache Management page with a successful message') .')</p>';
+        <p><strong>Diglin_UIOptimization Version: '.$version.'</strong></p><p>' . $block->toHtml() .' ('. $this->__('You will be redirected to the Cache Management page with a successful message') .')</p>';
     }
 }
